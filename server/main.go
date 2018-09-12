0148cb4a8140291c5b167877df8258f04324d56a
Matt Randell <matt@hexaglow.com>
2018-09-12 22:57:06 +0100

    repro issue without grpc

--- server/main.go
+++ server/main.go
@@ -1,36 +1,9 @@
 package main
 
 import (
-	"log"
-	"net"
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 	pb "hexaglow.com/demo/grpc/demo_service"
 )
 
-const (
-	port = ":1234"
-)
-
-type server struct{}
-
-func (s *server) Echo(ctx context.Context, in *pb.EchoMessage) (*pb.EchoMessage, error) {
-	return in, nil
-}
-
-
 func main() {
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	pb.RegisterDemoServiceServer(s, &server{})
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
-	log.Printf("Trying to listen on %s", port)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	var _ *pb.EchoMessage
 }
